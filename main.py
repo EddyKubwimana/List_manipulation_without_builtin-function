a34210d63e0cc8189df362df6a3f4c7880c6d1dc
EddyKubwimana <kubwimanaeddy1@gmail.com>
2022-11-06 13:31:27 +0000

    additional merge sort

--- main.py
+++ main.py
@@ -25,6 +25,31 @@ def selection_sort (listname):
                 listname[index], listname[item] = listname[item], listname[index]
         index = index+1
     return listname
+#merge sort algorithm
+def merge(right, left):
+    r,l = 0, 0
+    sorted_list = []
+    while r < len(right) and l < len(left):
+        if right[r] > left[l]:
+            sorted_list.append(left[l])
+            l += 1
+        else:
+            sorted_list.append(right[r])
+            r += 1
+    while r < len(right):
+        sorted_list.append(right[r])
+
+    while r < len(left):
+        sorted_list.append(left[r])
+    return sorted_list
+def merge_sort(listname):
+    if len(listname) < 2:
+        return listname
+    else :
+      middle = len(listname)//2
+      first_half = merge_sort(listname[:middle])
+      second_half = merge_sort(listname[middle:])
+      return merge(first_half, second_half)
 
 #time complexity for sort algorithm
 
@@ -41,5 +66,10 @@ print(time.time() - tim2)
 tim3 = time.time()
 print(selection_sort(number))
 print(time.time() - tim3)
+
+#time complexity for merge_sort
+tim4 = time.time()
+print(merge_sort(number))
+print(time.time() - tim4)
 
 
