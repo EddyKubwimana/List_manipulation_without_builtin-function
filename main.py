2486a10be0b7a63dbf96f85c9265228d6b44179c
EddyKubwimana <kubwimanaeddy1@gmail.com>
2022-11-06 12:54:23 +0000

    added two different algorithm to sort( selection_sort and bubble sort

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# sort a list without a built in function
+# sort a list without a built-in function
 import time
 
 timing = time.time()
 
-
+#first function with which focus on comparing two paralel numbers
 def sort(listname):
     swap = False
     while not swap:
@@ -16,7 +16,17 @@ def sort(listname):
                 listname[i] = listname[i - 1]
                 listname[i - 1] = swapper
     return listname
+# second algorithm focus on finding a smallest number and arrange the rest compared to that number
+def selection_sort (listname):
+    index = 0
+    while index != len(listname):
+        for item in range(index, len(listname)):
+            if listname[item] < listname[index]:
+                listname[index], listname[item] = listname[item], listname[index]
+        index = index+1
+    return listname
 
+#time complexity for sort algorithm
 
 number = [100, 200, 4, 10, -4, 76, 868, 200, 547, 5467, 54, 1, 4.6, 0, 10]
 print(sort(number))
@@ -26,5 +36,10 @@ print(time.time() - timing)
 tim2 = time.time()
 print(sorted(number))
 print(time.time() - tim2)
+
+#time complexity for selection algorithm
+tim3 = time.time()
+print(selection_sort(number))
+print(time.time() - tim3)
 
 
